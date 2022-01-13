d44df548028fd98ea3efc7110deb6c198a8d4ecd
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2022-01-13 09:49:09 +0800

    Update popup.js

--- ZhixuewangScoreExtension/popup/popup.js
+++ ZhixuewangScoreExtension/popup/popup.js
@@ -1,11 +1,22 @@
 const generate = document.getElementById("generate");
-if (generate) {
-  generate.onclick = function () {
-    console.log("Available")
+const checkbox = document.getElementById("checkbox");
+checkbox.onclick = function () {
+    const value = checkbox.checked
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        image_url: chrome.extension.getURL("images/fullmark_analyse.png")
-      }, function (responce) { });
-    })
-  }
+        chrome.tabs.sendMessage(tabs[0].id, {
+            type:"RealModeStatusChanged",
+            image_url: chrome.extension.getURL("images/fullmark_analyse.png")
+        }, function (responce) { });
+      })
+}
+if (generate) {
+    generate.onclick = function () {
+        console.log("Available")
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type:"FullMarkCallback",
+                image_url: chrome.extension.getURL("images/fullmark_analyse.png")
+            }, function (responce) { });
+        })
+    }
 }
